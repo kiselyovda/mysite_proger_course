f3120c8b038e02b912e9e8cd1a9c5b6731e940e6
kiselyovda <kiselyovda@ya.ru>
2022-05-01 00:34:47 +0300

    remove test view and reconstruct index view

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from .models import News
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world!')
-
-
-def test(request):
-    return HttpResponse('<h1 style="color: red; border: 4mm ridge rgba(211, 220, 50, .6);"><center>Тестовая надпись</center></h1>')
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, 'news/index.html', context)
