5edb25a7d4e440e3b633fb3b7e3a43d030b69578
kiselyovda <kiselyovda@ya.ru>
2022-05-04 07:33:50 +0300

    add categories

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-from .models import News
+from .models import News, Category
 
 
 # Create your views here.
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
+        'categories': categories,
     }
-    return render(request, 'news/index.html', context)
+    return render(request, 'news/index.html', context=context)
