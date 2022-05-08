52cbeaa62679f81b042b02d95d526d7f134586cc
kiselyovda <kiselyovda@ya.ru>
2022-05-08 21:51:15 +0300

    refactor functions

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,27 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import News, Category
 
 
 # Create your views here.
 def index(request):
-    news = News.objects.all()
-    categories = Category.objects.all()
     context = {
-        'news': news,
+        'news': News.objects.all(),
         'title': 'Список новостей',
-        'categories': categories,
     }
     return render(request, 'news/index.html', context=context)
 
 
 def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
-    category = Category.objects.get(pk=category_id)
     context = {
-        'news': news,
+        'news': News.objects.filter(category_id=category_id),
         'title': 'Список новостей',
-        'categories': categories,
+        'category': Category.objects.get(pk=category_id),
     }
     return render(request, 'news/category.html', context=context)
+
+
+def view_news(request, news_id):
+    # news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item': news_item})
