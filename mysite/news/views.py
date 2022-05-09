127092dc083d3f08b24ca4c78114fd040d24ec63
kiselyovda <kiselyovda@ya.ru>
2022-05-09 17:12:57 +0300

    add news from form

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
+from .forms import NewsForm
 
 
 # Create your views here.
@@ -22,6 +23,16 @@ def get_category(request, category_id):
 
 
 def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {'news_item': news_item})
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
+    else:
+        form = NewsForm()
+    return render(request, 'news/add_news.html', {'form': form})
